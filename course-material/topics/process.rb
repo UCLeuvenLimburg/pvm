02bbf062e376e0c42362858ce7348be6b83f251f
Frederic Vogels <frederic.vogels@ucll.be>
2020-03-14 22:02:33 +0100

    course-material/topics

--- course-material/topics/process.rb
+++ course-material/topics/process.rb
@@ -6,8 +6,8 @@ Find.find('.').select do |entry|
 end.each do |entry|
   contents = IO.read(entry)
 
-  contents.gsub!( /documentclass{pvm-slides}/, 'documentclass{../pvm-slides}' )
-  contents.gsub!( /documentclass\[handout\]{pvm-slides}/, 'documentclass[handout]{../pvm-slides}' )
+  contents.gsub!( /documentclass{pvm-slides}/, 'documentclass{../ucll-slides}' )
+  contents.gsub!( /documentclass\[handout\]{pvm-slides}/, 'documentclass[handout]{../ucll-slides}' )
 
   IO.write(entry, contents)
 end
